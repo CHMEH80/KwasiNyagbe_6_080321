37597dfd4c3c3796582a33ccf22673a488b50950
GG <nkgaloo@hotmail.fr>
2021-03-12 13:27:21 +0100

    Création controllers, middlewares, models

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,44 @@
-function set(arg1,arg2){
-  return true;
-}
 
-module.exports.set = set;
+// Plugin Npm Node.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+const path = require('path');
+
+// Connection à la base de donnée Mongoose
+mongoose.connect('mongodb+srv://Chmeh:Shlomo91@cluster0.k4k9j.mongodb.net/OcP6?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+const Sauce = require('./models/Sauce');
+const User = require('./models/User');
+
+// Routes 
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+// Utilisation du Framework Express
+const app = express();
+
+// Middleware pour les headers de requêtes et éviter les erreurs CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Traitement des sonnées par BodyParser 
+app.use(bodyParser.json());
+// Chemin virtuel pour les fichiers statiques tel que nos images
+app.use('/images', express.static(path.join(__dirname, 'images')));
+// Url des routes
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+
+module.exports = app;
